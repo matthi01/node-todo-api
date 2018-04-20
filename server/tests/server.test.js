d73013388b705730e50a393942ddf7312293a26f
matthi01 <mruhland01@gmail.com>
2018-04-20 14:00:46 -0700

    added test for logout route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -297,3 +297,25 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+
+describe('DELETE /users/me/token', () => {
+
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', seedUsersData[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err) {
+                    done(err);
+                }
+
+                User.findById(seedUsersData[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+});
