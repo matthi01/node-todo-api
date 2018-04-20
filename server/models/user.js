e1f6c954a4bbdb31016792e0cca5f37e3ca8e488
matthi01 <mruhland01@gmail.com>
2018-04-20 13:13:57 -0700

    added login path at POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -65,7 +65,26 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   });
+};
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
 };
 
 // going to override this method - don't want to return all the information in this model (like the password)
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const {User} = require('./models/user');
 const {authenticate} = require('./middleware/authenticate');
 
 
+
 const app = express();
 const port = process.env.PORT;
 
@@ -105,6 +106,7 @@ app.patch('/todos/:id', (req, res) => {
 
 // *********** USER ROUTES ***********
 
+// POST /users
 app.post('/users', (req, res) => {
   const body = _.pick(req.body, ['email', 'password']);
   const user = new User(body);
@@ -127,6 +129,22 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email','password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
